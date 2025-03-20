a54b2f7beb3761d4c402928444421d540a9ddc08
Pedro Ferreira <ferfonhp@gmail.com>
2025-03-20 18:41:27 -0300

    utilizando books service no books component

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../types/Book';
+import { BooksService } from './books.service';
 
 @Component({
   selector: 'app-books',
@@ -8,40 +9,18 @@ import { Book } from '../types/Book';
 })
 export class BooksComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor( private booksService: BooksService ) {
+    this.books = this.booksService.getBooks();
   }
 
-  textOnHtml: string = '';
+  ngOnInit(): void { }
 
+  books: Book[] = [];
+  textOnHtml: string = '';
   bookButtonDisabled = false;
   isShowing = true;
-
-
-  books: Book[] = [
-    {
-      name: 'entendendo algoritmos',
-      author: 'aditya y. bhargava',
-      img_path: 'https://m.media-amazon.com/images/I/71Vkg7GfPFL._SY425_.jpg',
-      price: 200
-    },
-    {
-      name: 'código limpo',
-      author: 'robert c. martin',
-      img_path: 'https://m.media-amazon.com/images/I/71dH97FwGbL._SY385_.jpg',
-      price: 350
-    },
-    {
-      name: 'javascript: o guia definitivo',
-      author: 'david flanagan',
-      img_path: 'https://m.media-amazon.com/images/I/91z1xY4ppaL._SY425_.jpg',
-      price: 450
-    }
-  ];
 
   addToCart(event: Book){
     console.log(event);
   }
-
 }
